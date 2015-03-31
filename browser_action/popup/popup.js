f12a5a9ecd8a9c89579aac719b521302086a6113
Peter Toth <tothpeter08@gmail.com>
2015-03-31 23:26:48 +0200

    2.2-Building a Browser Action

---
+++ browser_action/popup/popup.js
@@ -0,0 +1,9 @@
+(function(){
+  'use strict';
+
+  var backgroundPage = chrome.extension.getBackgroundPage();
+
+  document.querySelector('button').addEventListener('click', function() {
+    backgroundPage.handleButtonClick();
+  });
+})();
